be84c3ff239116d5d5849d16910bbc0e9d18c91a
Tom Hayward <tom@tomh.us>
2019-07-21 10:02:03 -0700

    DRY ssh command

--- backup.sh
+++ backup.sh
@@ -4,6 +4,9 @@
 
 DIR=$(dirname "${BASH_SOURCE[0]}")/backup
 LIMIT=8
+COMMON_OPTS="-o ConnectTimeout=10 -o BatchMode=yes"
+SCP_OPTS="$COMMON_OPTS"
+SSH_OPTS="$COMMON_OPTS -n"
 
 write_if_not_empty () {
 	head=$(dd bs=1 count=1 2>/dev/null; echo a)
@@ -19,11 +22,14 @@ cd "$DIR"
 while read router
 do
 	echo Backing up "$router"... 1>&2
-	ssh -n "$router" '/export hide-sensitive' \
+	SSH_CMD="ssh ${SSH_OPTS} $router"
+
+	$SSH_CMD '/export hide-sensitive' \
 	| sed 's![a-z]*/[0-3][0-9]/20[0-9][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9]!mm/dd/yyyy hh:mm:ss!' \
 	| write_if_not_empty "$router" &
 
-	ssh -n "$router" "/system backup save name=$router " && scp "$router":"$router".backup . &
+	$SSH_CMD "/system backup save name=$router " \
+	&& scp ${SCP_OPTS} "$router":"$router".backup . &
 
 	# only allow $LIMIT concurrent jobs
 	until [ $(jobs -p | wc -l) -lt $LIMIT ]
